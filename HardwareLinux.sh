33f010b02ec099ab3acea8d903fb760dd2aa5354
Elbissa02 <93919533+Elbissa02@users.noreply.github.com>
2023-06-13 16:41:37 +0200

    Create HardwareLinux.sh

---
+++ HardwareLinux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+echo "------Información relevante del procesador------"
+echo ""
+lscpu
+echo ""
+
+echo "------Información relevante de la memoria------"
+echo ""
+free
+echo""
+
+echo "------Información relevante del disco duro------"
+echo ""
+df
+echo ""
